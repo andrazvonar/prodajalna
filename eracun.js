a3e155ef6c85ba2c4c7b649a8a9a432841b67be0
Andraž Zvonar <az9677@student.uni-lj.si>
2016-05-03 19:32:07 +0000

    Implemetirana registracija

--- eracun.js
+++ eracun.js
@@ -208,14 +208,19 @@ streznik.post('/prijava', function(zahteva, odgovor) {
     	  Address, City, State, Country, PostalCode, \
     	  Phone, Fax, Email, SupportRepId) \
         VALUES (?,?,?,?,?,?,?,?,?,?,?,?)");
-      //TODO: add fields and finalize
-      //stmt.run("", "", "", "", "", "", "", "", "", "", "", 3); 
-      //stmt.finalize();
+      stmt.run(polja['FirstName'], polja['LastName'], polja['Company'],
+        polja['Address'], polja['City'], polja['State'], polja['Country'],
+        polja['PostalCode'], polja['Phone'], polja['Fax'], polja['Email'], 3); 
+      stmt.finalize();
     } catch (err) {
       napaka2 = true;
     }
-  
-    odgovor.end();
+    
+    vrniStranke(function(napaka1, stranke) {
+      vrniRacune(function(napaka2, racuni) {
+        odgovor.render('prijava', {sporocilo: "Stranka je bila uspešno registrirana", seznamStrank: stranke, seznamRacunov: racuni});  
+      }) 
+    });
   });
 })
 
